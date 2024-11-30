a66eaa8646e97804fe10b262475315210c1c6c59
tuoreste <tuoreste@gmail.com>
2024-11-30 12:30:32 +0100

    Func: in handlers, defined to handle requests that fetch info destination of continent, plus func to get a list of all continents

--- backend/handlers/destination_handlers.go
+++ backend/handlers/destination_handlers.go
@@ -1,1 +1,38 @@
 package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/tuoreste/API-Travel-Destinations.git/models"
+)
+
+//handling requests to fetch destinations by continent
+func	GetDestinationsByContinent(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	continent := vars["continent"]
+
+	destinations := models.GetDestinationsByContinent(continent)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-control-Allow-Origin", "*")
+	json.NewEncoder(w).Encode(destinations)
+}
+
+//get all continents
+func	GetAllContinents(w http.ResponseWriter, r *http.Request) {
+	continents := []string{
+		"North America",
+		"South America",
+		"Europe",
+		"Asia",
+		"Africa",
+		"Australia",
+		"Antarctica",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	json.NewEncoder(w).Encode(continents)
+}
